9792d3dd0d054248a7e592943e0d8264b8063cb3
Rayan581 <ra3328600@gmail.com>
2024-10-14 18:03:00 +0500

    Added the grid and placed components on the grid

--- AI_B_i230018/main.cpp
+++ AI_B_i230018/main.cpp
@@ -5,9 +5,17 @@ using namespace std;
 
 int main()
 {
-    Grid grid(5);
+    initscr();            // Start ncurses mode
+    cbreak();             // Disable line buffering
+    noecho();             // Don't echo input to the screen
+    keypad(stdscr, TRUE); // Enable function keys
+
+    Grid grid(10);
+    grid.initialize_grid(1);
 
     grid.print();
+
+    endwin();
 
     return 0;
 }
