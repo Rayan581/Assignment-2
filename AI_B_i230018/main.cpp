a8b5c85a28ae50a9c45279e322dd1c29301ba8e8
Rayan581 <ra3328600@gmail.com>
2024-10-20 16:38:35 +0500

    -Added a prompt for the user to select difficulty level

--- AI_B_i230018/main.cpp
+++ AI_B_i230018/main.cpp
@@ -5,11 +5,20 @@ using namespace std;
 
 int main()
 {
-    initscr();            // Start ncurses mode
-    cbreak();             // Disable line buffering
-    noecho();             // Don't echo input to the screen
+    initscr(); // Start ncurses mode
+    cbreak();  // Disable line buffering
+    noecho();  // Don't echo input to the screen
 
-    Game game(1);
+    // Prompt the user to select the difficulty level
+    printw("Select difficulty level:\n");
+    printw("1. Easy\n");
+    printw("2. Medium\n");
+    printw("3. Hard\n");
+    printw("Enter number to select: ");
+    int level = (getch() - '0');
+    clear();
+
+    Game game(level);
     game.game_loop();
 
     getch();
